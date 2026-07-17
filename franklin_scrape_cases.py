7d460eb949952697ae3be3ae7629163cebb3755d
Khadimul Talukder <khadimultalukder@gmail.com>
2026-07-18 00:49:58 +0600

    Update franklin_scrape_cases.py

--- franklin_scrape_cases.py
+++ franklin_scrape_cases.py
@@ -198,8 +198,7 @@ async def main():
         await human_wait(1, 2)
         await click_ok_if_present(page)
         await human_wait(0.5, 2)
-        await click_ok_if_present(page)
-        await human_wait(0.5, 2)
+
 
         await page.goto(CALENDAR_URL)
         await human_wait(1.5, 3)
